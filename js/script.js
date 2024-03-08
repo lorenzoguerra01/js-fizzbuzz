1317f4d325ac7b1e03b52cd70b29a39956dd546a
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-03-08 16:59:50 +0100

    add js conditions, css colors, edit style

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 const squareWrapper = document.createElement('div');
 //console.log(typeof squareWrapper);
 
-squareWrapper.className = 'd-flex justify-content-between align-content-center flex-wrap';
-squareWrapper.setAttribute('id','boxes');
+squareWrapper.className = 'd-flex align-content-center flex-wrap ms_gap';
+squareWrapper.id = "boxes";
 //console.dir(squareWrapper);
 
 let numBox = 100;
 let tmpHtml = '';
 
-for(let i = 0; i < numBox; i++){
-    tmpHtml += ` <div class="box">${i + 1}</div>`;
+for (let i = 0; i < numBox; i++) {
+    if (((i + 1) % 3 === 0) && ((i + 1) % 5 === 0)) {
+        tmpHtml += ` <div class="box box-fizzbuzz">fizzbuzz</div>`;
+    } else if ((i + 1) % 3 === 0) {
+        tmpHtml += ` <div class="box box-fizz">fizz</div>`;
+    } else if ((i + 1) % 5 === 0) {
+        tmpHtml += ` <div class="box box-buzz">buzz</div>`;
+    } else {
+        tmpHtml += ` <div class="box box-standard">${i + 1}</div>`;
+    }
+    console.log(tmpHtml);
 }
 
 squareWrapper.innerHTML = tmpHtml;
